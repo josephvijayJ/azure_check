1d8a7eb232f7d2f174f024f8e80c475a1642e53f
josephvijayJ <josephvijayj17bca015@gmail.com>
2022-09-14 16:48:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('HELLO WORLD');
+});
+app.listen(5000, () => {
+  console.log('connected to port');
+});
